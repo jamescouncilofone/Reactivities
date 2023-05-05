f609093914992c5d88e49faa34fcdbe0ee6f924e
JFitzwater <darthyeti@gmail.com>
2023-05-05 14:11:33 -0400

    End of section 12

---
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,51 @@
+using System.Text;
+using API.Services;
+using Domain;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Persistence;
+
+namespace API.Extensions
+{
+    public static class IdentityServiceExtensions
+    {
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services, 
+            IConfiguration config)
+            {
+                services.AddIdentityCore<AppUser>(opt => {
+                    opt.Password.RequireNonAlphanumeric = false;
+                    opt.User.RequireUniqueEmail = true;
+                })
+                .AddEntityFrameworkStores<DataContext>();
+
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+
+                services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(opt => 
+                    {
+                        opt.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            ValidateIssuerSigningKey = true,
+                            IssuerSigningKey = key,
+                            ValidateIssuer = false,
+                            ValidateAudience = false
+                        };
+                    });
+
+
+
+                // AddScopted means that this token service is going to be scoped to the HTTP request
+                // itself. So when the HTTP request comes in we go to our account controller and 
+                // request a token because we are attempting to login, then a new instance of our
+                // token service will be created and when the HTTP request is finished then we will
+                // dispose of TokenService.
+                //
+                // NOTE: services.AddTransient - scope to method.
+                //       services.AddSingleton - creates the service when application starts and is
+                //                               kept alive unitl the application shuts down.
+                services.AddScoped<TokenService>();
+
+                return services;
+            }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,9 @@
 using API.Extensions;
 using API.Middleware;
+using Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -8,8 +12,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(opt =>
+{
+    var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+    opt.Filters.Add(new AuthorizeFilter(policy));
+});
+
 builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
@@ -28,6 +38,8 @@ if (app.Environment.IsDevelopment())
 // CORS needs to happen before Authenication.
 app.UseCors("CorsPolicy");
 
+// IMPORTANT: UseAuthentication middleware MUST be specified BEFORE UseAuthorization.
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
@@ -39,8 +51,9 @@ var services = scope.ServiceProvider;
 try
 {
     var context = services.GetRequiredService<DataContext>();
+    var userManager = services.GetRequiredService<UserManager<AppUser>>();
     await context.Database.MigrateAsync();
-    await Seed.SeedData(context);
+    await Seed.SeedData(context, userManager);
 }
 catch (Exception ex)
 {
