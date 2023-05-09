790eb1dc06f1e3703934bbd5b3b06beaa5165b8f
JFitzwater <darthyeti@gmail.com>
2023-05-09 19:06:32 -0400

    End of section 14

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -1,7 +1,9 @@
 using System.Text;
 using API.Services;
 using Domain;
+using Infrastructure.Security;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.IdentityModel.Tokens;
 using Persistence;
 
@@ -32,7 +34,13 @@ namespace API.Extensions
                         };
                     });
 
+                services.AddAuthorization(opt => {
+                    opt.AddPolicy("IsActivityHost", policy => {
+                        policy.Requirements.Add(new IsHostRequirement());
+                    });
+                });
 
+                services.AddTransient<IAuthorizationHandler, IsHostRequirementHandler>();
 
                 // AddScopted means that this token service is going to be scoped to the HTTP request
                 // itself. So when the HTTP request comes in we go to our account controller and 
